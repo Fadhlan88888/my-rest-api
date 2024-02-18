07cc80bcceaefb24d3f29dfaf978d634fc55f759
Fadhlan777 <85679972+Fadhlan88888@users.noreply.github.com>
2024-02-18 03:42:36 -0500

    settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "mongodb+srv://Fadhlan78:5OkmgQP8dlzbkHov@cluster0.rsd4nnw.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://xoru78:4fZhqudvs0JoASKz@cluster0.k0eznma.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "fadhla.games@gmail.com" //email
 global.email_password = "wxkg veps wxux bgka" //application password email
